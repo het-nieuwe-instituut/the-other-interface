a1890666f200b8a3257e75238016d6ec10bcd9b6
rick.woltheus@lifely.nl <rick.woltheus@lifely.nl>
2022-08-29 14:20:36 +0200

    feat(): add translation library

---
+++ nextjs/src/common/utils/translations.ts
@@ -0,0 +1,9 @@
+import setLanguage from 'next-translate/setLanguage'
+
+type Join<S1, S2> = S1 extends string ? (S2 extends string ? `${S1}.${S2}` : never) : never
+
+export type Paths<T> = {
+    [K in keyof T]: T[K] extends Record<string, unknown> ? Join<K, Paths<T[K]>> : K
+}[keyof T]
+
+export const setAppLanguage = setLanguage
