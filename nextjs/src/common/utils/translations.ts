bea2b6f4ec0d703e67206776aff292ee3c48052f
rick.woltheus@lifely.nl <rick.woltheus@lifely.nl>
2022-08-29 14:54:41 +0200

    feat(): add support for outside react translations

--- nextjs/src/common/utils/translations.ts
+++ nextjs/src/common/utils/translations.ts
@@ -1,4 +1,7 @@
 import setLanguage from 'next-translate/setLanguage'
+import getT from 'next-translate/getT'
+import { TranslationKeys } from 'locales/locales'
+import { TranslationQuery } from 'next-translate'
 
 type Join<S1, S2> = S1 extends string ? (S2 extends string ? `${S1}.${S2}` : never) : never
 
@@ -7,3 +10,17 @@ export type Paths<T> = {
 }[keyof T]
 
 export const setAppLanguage = setLanguage
+
+export const getTranslation = async <T extends keyof TranslationKeys>(locale: string, ns: T) => {
+    const t = await getT(locale, ns)
+
+    return (
+        s: TranslationKeys[T],
+        q?: TranslationQuery,
+        o?: {
+            returnObjects?: boolean
+            fallback?: string | string[]
+            default?: string
+        }
+    ) => t(s, q, o)
+}
--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -1,13 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect } from 'react'
 import { useTypeSafeTranslation } from 'src/common/hooks/translations'
-import { setAppLanguage } from 'src/common/utils/translations'
+import { getTranslation, setAppLanguage } from 'src/common/utils/translations'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
     const { t, lang } = useTypeSafeTranslation('common')
     const example = t('commonTest', { count: 12 })
+
+    useEffect(() => {
+        async function t() {
+            const t = await getTranslation('nl', 'common')
+            console.log(t('commonTest'))
+        }
+        t()
+    })
 
     return (
         <div className={styles.container}>
@@ -21,6 +30,7 @@ const Home: NextPage = () => {
                 <button onClick={async () => await setAppLanguage('en')}>EN</button>
                 <button onClick={async () => await setAppLanguage('nl')}>NL</button>
                 <h1 className={styles.title}>{example}</h1>
+                <h1 className={styles.title}>{}</h1>
             </main>
         </div>
     )
