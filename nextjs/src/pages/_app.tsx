bd37a41caca740a20af2a86634cda34ccce8d0f3
koenwisse <koen@launchme.eu>
2022-08-31 10:18:36 +0200

    HNIT-273 setup css library and chakra UI
    first part install and setup chakra

--- nextjs/src/pages/_app.tsx
+++ nextjs/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'react-redux'
 
 import store from '../features/shared/configs/store'
@@ -7,7 +8,9 @@ import store from '../features/shared/configs/store'
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <Provider store={store}>
-            <Component {...pageProps} />
+            <ChakraProvider>
+                <Component {...pageProps} />
+            </ChakraProvider>
         </Provider>
     )
 }
