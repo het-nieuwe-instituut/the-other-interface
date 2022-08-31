1c0b5c8aeb690116c0edc3f7568dd6a991ce0e6b
Gorkem Kockaya <gorkem.kockaya@gmail.com>
2022-08-31 20:50:27 +0300

    feat(strapi-module): add module

--- gateway-app/src/app.module.ts
+++ gateway-app/src/app.module.ts
@@ -6,9 +6,13 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AppResolver } from './app.resolver'
 import { HttpModule } from '@nestjs/axios'
+import { StoryModule } from './modules/story/story.module'
+import { StrapiModule } from './modules/strapi/strapi.module'
 
 @Module({
     imports: [
+        StoryModule,
+        StrapiModule,
         ConfigModule.forRoot({ isGlobal: true }),
         GraphQLModule.forRoot<ApolloDriverConfig>({
             driver: ApolloDriver,
