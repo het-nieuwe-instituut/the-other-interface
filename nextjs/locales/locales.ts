ced86874bab02c0d4d01075a6b9dea3259f28cf3
rick.woltheus@lifely.nl <rick.woltheus@lifely.nl>
2022-09-02 11:28:22 +0200

    move common to shared

--- nextjs/locales/locales.ts
+++ nextjs/locales/locales.ts
@@ -1,5 +1,6 @@
 // we use english by default, so types will be taken from there
-import { Paths } from 'src/common/utils/translations'
+
+import { Paths } from 'src/features/shared/utils/translations'
 import common from './en/common.json'
 
 export type TranslationKeys = {
