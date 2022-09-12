b33978b787b2d117ae12ba16b779a0689bf644a4
rick.woltheus@lifely.nl <rick.woltheus@lifely.nl>
2022-09-12 13:23:21 +0200

    feat(Nextjs): add graphql homepage query

--- nextjs/src/features/hello/hello.tsx
+++ nextjs/src/features/hello/hello.tsx
@@ -1,9 +1,11 @@
-import { useHelloQuery } from 'src/generated/graphql'
+// import { useHelloQuery } from 'src/generated/graphql'
 
 export const Hello = () => {
-    const { data, loading, error } = useHelloQuery()
+    // const { data, loading, error } = useHelloQuery()
 
-    if (error) return <p>{error.message}</p>
-    if (loading) return <p>loading</p>
-    return <p>{data?.hello}</p>
+    // if (error) return <p>{error.message}</p>
+    // if (loading) return <p>loading</p>
+    // return <p>{data?.hello}</p>
+
+    return 'hello'
 }
--- nextjs/src/mocks/handlers.ts
+++ nextjs/src/mocks/handlers.ts
@@ -1,13 +1,11 @@
-import { mockHelloQuery } from '../generated/graphql'
 import { setupServer } from 'msw/node'
-import { aQuery } from './generated-mocks'
 
-export const mockServer = setupServer(
-    mockHelloQuery((req, res, ctx) => {
-        return res(
-            ctx.data({
-                hello: aQuery().hello,
-            })
-        )
-    })
-)
+export const mockServer = setupServer()
+
+// mockHelloQuery((req, res, ctx) => {
+//     return res(
+//         ctx.data({
+//             hello: aQuery().hello,
+//         })
+//     )
+// })
