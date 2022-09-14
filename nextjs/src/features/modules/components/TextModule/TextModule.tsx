b666369829c0bf60a0fc4c693657831393e6b8c6
rick.woltheus@lifely.nl <rick.woltheus@lifely.nl>
2022-09-14 16:20:16 +0200

    add spacing

--- nextjs/src/features/modules/components/TextModule/TextModule.tsx
+++ nextjs/src/features/modules/components/TextModule/TextModule.tsx
@@ -1,6 +1,7 @@
 import ReactMarkdown from 'react-markdown'
 import { ComponentModulesTextModule } from 'src/generated/graphql'
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer'
+import { Box } from '@chakra-ui/react'
 
 interface Props {
     component: ComponentModulesTextModule
@@ -13,8 +14,14 @@ export const TextModule: React.FC<Props> = props => {
         return null
     }
     return (
-        <>
+        <Box
+            pl={6}
+            pr={6}
+            maxWidth={'820px'}
+            mb={props.component.spacingBottom ?? undefined}
+            mt={props.component.spacingTop ?? undefined}
+        >
             <ReactMarkdown components={ChakraUIRenderer()}>{content}</ReactMarkdown>
-        </>
+        </Box>
     )
 }
