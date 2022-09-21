eecd625742b764f17cd7d05fe6a63aef51681b4c
rick.woltheus@lifely.nl <rick.woltheus@lifely.nl>
2022-09-21 09:09:23 +0200

    fix jest

--- nextjs/src/features/pages/containers/LandingpageContainer/LandingpageContainer.spec.tsx
+++ nextjs/src/features/pages/containers/LandingpageContainer/LandingpageContainer.spec.tsx
@@ -1,6 +1,7 @@
 import { mockedClient } from '@/features/graphql/config/mockedApollo'
 import { renderWithProviders } from '@/features/shared/utils/test-utils'
 import { mockLandingpageBySlugQuery } from 'src/generated/graphql'
+import { aComponentModulesTextModule } from 'src/mocks/generated-mocks'
 import { mockServer } from 'src/mocks/handlers'
 import { LandingpageContainer } from './LandingpageContainer'
 jest.mock('next/router', () => ({
@@ -26,7 +27,7 @@ describe('LandingpageContainer', () => {
         const { findByText, getByText } = renderWithProviders(<LandingpageContainer />)
 
         getByText('loading')
-        await findByText('incidunt')
+        await findByText(aComponentModulesTextModule().Richtext ?? '')
     })
 
     it('should display error', async () => {
