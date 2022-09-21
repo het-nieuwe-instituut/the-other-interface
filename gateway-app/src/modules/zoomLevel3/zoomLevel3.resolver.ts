8f4697e3eb40fcf328c69df07c24a85686f42230
Joran de Waaij <joran@lifely.nl>
2022-09-21 10:05:41 +0200

    fix(resolver): fix naming

--- gateway-app/src/modules/zoomLevel3/zoomLevel3.resolver.ts
+++ gateway-app/src/modules/zoomLevel3/zoomLevel3.resolver.ts
@@ -6,7 +6,7 @@ import { PaginationArgs, ZoomLevel3Args, ZoomLevel3Type } from './zoomLevel3.typ
 export class ZoomLevel3Resolver {
     public constructor(private readonly zoomLevel3Service: ZoomLevel3Service) {}
     @Query(() => [ZoomLevel3Type])
-    public async filterOptions(@Args() args: ZoomLevel3Args, @Args() paginationArgs: PaginationArgs) {
+    public async zoomLevel3(@Args() args: ZoomLevel3Args, @Args() paginationArgs: PaginationArgs) {
         return this.zoomLevel3Service.getData(args.entityName, args.filterId, paginationArgs)
     }
 }
