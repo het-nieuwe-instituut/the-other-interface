b7da40b48652ce53ea46c7271acb5c773fee0c57
rick.woltheus@lifely.nl <rick.woltheus@lifely.nl>
2022-09-21 12:56:08 +0200

    fix: refactor inline logic

--- nextjs/src/features/modules/components/ButtonsModule/ButtonsModule.tsx
+++ nextjs/src/features/modules/components/ButtonsModule/ButtonsModule.tsx
@@ -1,7 +1,7 @@
 import { keyExtractor } from '@/features/shared/utils/lists'
 import { Box, Button, Flex } from '@chakra-ui/react'
 import NextLink from 'next/link'
-import { ComponentModulesButtonsModule } from 'src/generated/graphql'
+import { ComponentCoreButton, ComponentModulesButtonsModule } from 'src/generated/graphql'
 import ExternalLink from '@/icons/arrows/external-link.svg'
 import { isExternalURL } from '@/features/shared/utils/links'
 
@@ -21,19 +21,18 @@ export const ButtonsModule: React.FC<Props> = props => {
     function renderButtons() {
         return props.component.buttons?.map((button, index, array) => (
             <NextLink key={keyExtractor(button, index, array)} href={button?.url ?? ''} passHref>
-                <Button
-                    as={'a'}
-                    rightIcon={
-                        button?.url ? (
-                            isExternalURL(button?.url) ? (
-                                <ExternalLink color={'currentColor'} />
-                            ) : undefined
-                        ) : undefined
-                    }
-                >
+                <Button as={'a'} rightIcon={renderExternalLink(button)}>
                     {button?.text}
                 </Button>
             </NextLink>
         ))
+    }
+
+    function renderExternalLink(button?: ComponentCoreButton | null) {
+        if (!button?.url || !isExternalURL(button.url)) {
+            return undefined
+        }
+
+        return <ExternalLink color={'currentColor'} />
     }
 }
