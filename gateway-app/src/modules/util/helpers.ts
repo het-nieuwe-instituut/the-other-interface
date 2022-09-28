56e1951d68d76bc4391b483a8a717aa0e4830259
Gorkem Kockaya <gorkem.kockaya@gmail.com>
2022-09-28 14:57:34 +0300

    feat: add utils & helpers

---
+++ gateway-app/src/modules/util/helpers.ts
@@ -0,0 +1,20 @@
+import { random } from 'lodash'
+
+export function getRandomItemFromArray<T>(arr: T[]) {
+    if (!arr.length) {
+        return []
+    }
+
+    if (arr.length === 2) {
+        return arr
+    }
+
+    const cp = [...arr]
+    const rand1 = random(1, cp.length - 1)
+    const random1 = cp.splice(rand1 - 1, rand1)
+
+    const rand2 = random(1, cp.length - 1)
+    const random2 = cp.splice(rand2 - 1, rand2)
+
+    return [random1, random2]
+}
