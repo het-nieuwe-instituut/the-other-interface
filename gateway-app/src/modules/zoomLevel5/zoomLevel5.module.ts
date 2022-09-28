89fe5baf7c7185c07f81ab3c515b68e33866473a
Joran de Waaij <joran@lifely.nl>
2022-09-28 15:47:44 +0200

    feat(archives): implement zoom level 5

--- gateway-app/src/modules/zoomLevel5/zoomLevel5.module.ts
+++ gateway-app/src/modules/zoomLevel5/zoomLevel5.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { ArchivesModule } from '../archives/archives.module'
 import { ObjectsModule } from '../objects/objects.module'
 import { PeopleModule } from '../people/people.module'
 import { PublicationsModule } from '../publications/publications.module'
@@ -6,7 +7,7 @@ import { RelatedRecordTypeResolver, ZoomLevel5Resolver } from './zoomLevel5.reso
 import { ZoomLevel5Service } from './zoomLevel5.service'
 
 @Module({
-    imports: [ObjectsModule, PeopleModule, PublicationsModule],
+    imports: [ObjectsModule, PeopleModule, PublicationsModule, ArchivesModule],
     providers: [ZoomLevel5Service, ZoomLevel5Resolver, RelatedRecordTypeResolver],
 })
 export class ZoomLevel5Module {}
