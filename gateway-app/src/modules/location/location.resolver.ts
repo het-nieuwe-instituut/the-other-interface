e6ea4406bef02218368d100e941c01f060404cab
Joran de Waaij <joran@lifely.nl>
2022-10-03 16:30:40 +0200

    feat(location): add resolvers

--- gateway-app/src/modules/location/location.resolver.ts
+++ gateway-app/src/modules/location/location.resolver.ts
@@ -1,8 +1,9 @@
 import { Inject } from '@nestjs/common'
-import { Args, Query, Resolver } from '@nestjs/graphql'
+import { Args, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql'
 import { PublicationState, Sdk } from '../../generated/strapi-sdk'
 import { I18NLocaleCode, PaginationArg } from '../strapi/shared-types'
 import { LocationEntityResponse, LocationFiltersInput } from './location.type'
+import { Location } from './location-dependency.type'
 
 @Resolver()
 export class LocationResolver {
@@ -32,5 +33,34 @@ export class LocationResolver {
         })
 
         return res.locations
+    }
+}
+
+@Resolver(Location)
+export class LocationFieldResolver {
+    public constructor(@Inject('StrapiGqlSDK') private readonly strapiGqlSdk: Sdk) {}
+
+    @ResolveField()
+    public async stories(@Parent() location: Location) {
+        if (location.stories?.data && location.stories?.data.length) {
+            return this.strapiGqlSdk.stories({}) // todo
+        }
+        return []
+    }
+
+    @ResolveField()
+    public async localizations(@Parent() location: Location) {
+        if (location.localizations?.data && location.localizations.data.length) {
+            const res = await this.strapiGqlSdk.locations({
+                filters: {
+                    or: location.localizations.data.map(ent => {
+                        return { id: { eq: ent.id } }
+                    }),
+                },
+            })
+
+            return res.locations
+        }
+        return []
     }
 }
