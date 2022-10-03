9ee8f627e3129192241ebc89ca53bceea6689cbb
Gorkem Kockaya <gorkem.kockaya@gmail.com>
2022-10-03 18:33:23 +0300

    feat: resolve homepage

--- gateway-app/src/modules/homepage/homepage.resolver.ts
+++ gateway-app/src/modules/homepage/homepage.resolver.ts
@@ -1,8 +1,14 @@
 import { Inject } from '@nestjs/common'
-import { Resolver } from '@nestjs/graphql'
-import { Sdk } from 'src/generated/strapi-sdk'
+import { Args, Query, Resolver } from '@nestjs/graphql'
+import { PublicationState, Sdk } from '../../generated/strapi-sdk'
+import { HomepageEntityResponse } from './homepage.type'
 
 @Resolver()
 export class HomepageResolver {
     public constructor(@Inject('StrapiGqlSDK') private readonly strapiGqlSdk: Sdk) {}
+
+    @Query(() => HomepageEntityResponse)
+    public homepage(@Args('publicationState') publicationState: PublicationState, @Args('locale') locale: string) {
+        return this.strapiGqlSdk.homepage({ publicationState, locale })
+    }
 }
