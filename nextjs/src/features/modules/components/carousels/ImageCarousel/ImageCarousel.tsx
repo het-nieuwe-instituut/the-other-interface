db97aa92e1c60d1332dc1242260ac854c3d303e2
Mykyta Siryk <countrytramp@gmail.com>
2022-10-05 15:54:02 +0200

    add more console.logs

--- nextjs/src/features/modules/components/carousels/ImageCarousel/ImageCarousel.tsx
+++ nextjs/src/features/modules/components/carousels/ImageCarousel/ImageCarousel.tsx
@@ -6,7 +6,7 @@ import ArrowLeftIcon from '@/icons/arrows/arrow-left-long.svg'
 import ArrowRightIcon from '@/icons/arrows/arrow-right-long.svg'
 import Image from 'next/image'
 import usePresenter from './usePresenter'
-// import getConfig from 'next/config'
+import getConfig from 'next/config'
 import { imageBasePath } from '../../../modulesConstants'
 
 
@@ -21,7 +21,7 @@ export const ImageCarousel = (props: Props) => {
     const { images } = props.component
     const items = images?.data
     const { carouselRef, handlePaginationPrev, handlePaginationNext, sliderRef, calculateImagePropotions, size } = usePresenter(items)
-    // const { publicRuntimeConfig } = getConfig()
+    const { publicRuntimeConfig } = getConfig()
 
     return (
         <Box as="div" backgroundColor={'white'} ref={carouselRef} position="relative" pl={'6'}>
@@ -39,6 +39,7 @@ export const ImageCarousel = (props: Props) => {
                         const originalWidth = item?.attributes?.width ?? 1
                         // const imageBasePath = publicRuntimeConfig.NEXT_PUBLIC_REACT_APP_IMAGE_BASE_URL ?? ''
                         console.log(imageBasePath)
+                        console.log(publicRuntimeConfig)
                         const imagePath =  imageBasePath + item?.attributes?.url
                         const caption = item?.attributes?.caption
                         const proportions = calculateImagePropotions(originalWidth, originalHeight, IMAGE_HEIGHT, size)
