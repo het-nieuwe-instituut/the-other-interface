a49c8fd98f83919da6f6127ab925b051d1fa7008
rick.woltheus@lifely.nl <rick.woltheus@lifely.nl>
2022-10-05 15:03:38 +0200

    feat: add translations

--- nextjs/locales/locales.ts
+++ nextjs/locales/locales.ts
@@ -4,9 +4,11 @@ import { Paths } from 'src/features/shared/utils/translations'
 import common from './en/common.json'
 import stories from './en/stories.json'
 import homepage from './en/homepage.json'
+import landingpage from './en/landingpage.json'
 
 export type TranslationKeys = {
     common: Paths<typeof common>
     stories: Paths<typeof stories>
     homepage: Paths<typeof homepage>
+    landingpage: Paths<typeof landingpage>
 }
