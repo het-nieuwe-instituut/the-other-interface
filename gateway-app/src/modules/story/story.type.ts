c7890e5c1edbc0781a8651076067c6be700ce38c
Gorkem Kockaya <gorkem.kockaya@gmail.com>
2022-10-12 10:21:03 +0300

    fix: remove unnecessary relations

--- gateway-app/src/modules/story/story.type.ts
+++ gateway-app/src/modules/story/story.type.ts
@@ -1,16 +1,6 @@
 import { ObjectType, Field, createUnionType, ID, InputType } from '@nestjs/graphql'
-import { ArchivesFondsZoomLevel5DetailType, ArchivesOtherZoomLevel5DetailType } from '../archives/archives.type'
-
 import { AuthorEntityResponse, AuthorFiltersInput } from '../author/author.type'
 import { LocationFiltersInput, LocationRelationResponseCollection } from '../location/location.type'
-import { ObjectsZoomLevel5DetailType } from '../objects/objects.type'
-import { PoepleZoomLevel5DetailType } from '../people/people.type'
-import {
-    PublicationsArticleZoomLevel5DetailType,
-    PublicationsAudioVisualZoomLevel5DetailType,
-    PublicationsBookZoomLevel5DetailType,
-    PublicationsSerialZoomLevel5DetailType,
-} from '../publications/publications.type'
 import {
     ComponentCorePublicationDate,
     ComponentCorePublicationDateFiltersInput,
@@ -87,57 +77,7 @@ export class Story {
 
     @Field({ nullable: true })
     public updatedAt: Date
-
-    @Field(() => [PoepleZoomLevel5DetailType], { nullable: 'itemsAndList' })
-    public people?: PoepleZoomLevel5DetailType[]
-
-    @Field(() => [StoryArchivesUnionType], { nullable: 'itemsAndList' })
-    public archives?: typeof StoryArchivesUnionType[]
-
-    @Field(() => [ObjectsZoomLevel5DetailType], { nullable: 'itemsAndList' })
-    public objects?: ObjectsZoomLevel5DetailType[]
-
-    @Field(() => [StoryPublicationsUnionType], { nullable: 'itemsAndList' })
-    public publications?: typeof StoryPublicationsUnionType[]
 }
-
-export const StoryArchivesUnionType = createUnionType({
-    name: 'StoryArchivesUnionType',
-    types: () => [ArchivesFondsZoomLevel5DetailType, ArchivesOtherZoomLevel5DetailType] as const,
-    resolveType(value) {
-        // TODO: ask lois how to recognize the difference properly
-        if (value.hasOwnProperty('descriptionLevel')) {
-            return ArchivesOtherZoomLevel5DetailType
-        } else {
-            return ArchivesFondsZoomLevel5DetailType
-        }
-    },
-})
-
-export const StoryPublicationsUnionType = createUnionType({
-    name: 'StoryPublicationsUnionType',
-    types: () =>
-        [
-            PublicationsAudioVisualZoomLevel5DetailType,
-            PublicationsArticleZoomLevel5DetailType,
-            PublicationsSerialZoomLevel5DetailType,
-            PublicationsBookZoomLevel5DetailType,
-        ] as const,
-    resolveType(value) {
-        switch (value.typeOfPublicationLabel) {
-            case 'audio-visueel materiaal':
-                return PublicationsAudioVisualZoomLevel5DetailType
-            case 'tijdschriftartikel':
-                return PublicationsArticleZoomLevel5DetailType
-            case 'tijdschrift':
-                return PublicationsSerialZoomLevel5DetailType
-            case 'boek':
-                return PublicationsBookZoomLevel5DetailType
-            default:
-                return null
-        }
-    },
-})
 
 export const StoryComponentsDynamicZone = createUnionType({
     name: 'StoryComponentsDynamicZone',
