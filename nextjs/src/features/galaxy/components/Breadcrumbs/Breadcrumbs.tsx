6ba03a9fe795293d04d9ee65fae82100fd8a4fcc
Mykyta Siryk <countrytramp@gmail.com>
2022-10-19 13:30:00 +0200

    creare breadcrumbs data p1

--- nextjs/src/features/galaxy/components/Breadcrumbs/Breadcrumbs.tsx
+++ nextjs/src/features/galaxy/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,61 +1,88 @@
+import { isTypeNode } from 'graphql'
 import { useRouter } from 'next/router'
 import { ZoomLevel } from '../../types/galaxy'
 
-const typeToName: Record<string, string> = {
+const typeToNamePlural: Record<string, string> = {
     people: 'People',
-    publications: 'Publication',
-    objects: 'Object',
-    archives: 'Archive',
-    story: 'Stories'
+    publications: 'Publications',
+    objects: 'Objects',
+    archives: 'Archives',
+    stories: 'Stories'
 }
 
-const getLevel3 = (type: string) => {
+const getLevel2or3 = (type: string, filter?: string) => {
+    const typeToName: Record<string, string> = {
+        people: 'People filters',
+        publications: 'Publication filters',
+        objects: 'Object filters',
+        archives: 'Archive filters',
+        stories: 'Stories'
+    }
+
     return {
-        name: typeToName[type],
+        name: `${typeToName[type]}`,
         link : {
-            pathname: `/landingpage/${type}`
+            pathname: filter ? `/landingpage/${type}/${filter}` : `/landingpage/${type}`
         }
     }
 }
 
-const getBreadcrumbsFromUrl = (asPath: string, query?: string) => {
-    const zoomLevel = ZoomLevel.Zoom0
-    const pathArray = asPath.split('/')
-    // console.log(pathArray)
-    // create zoom lebel objects
-    // fore object whith deps create mappers
-    // than define zoom level 
-    // create mapper zoom level to [...level1, ...level2]
+const getLevel4 = (type: string, filter: string, filterType: string) => {
+    const typeToName: Record<string, string> = {
+        people: 'People',
+        publications: 'Publications',
+        objects: 'Objects',
+        archives: 'Archives',
+        stories: 'Stories'
+    }
 
-    // if (pathArray.includes('landingpage')){
+    return {
+        name: `${typeToName[type]}`,
+        link : {
+            pathname: `/landingpage/${type}/${filter}/${filterType}`
+        }
+    }
+}
 
-    // }
+const getBreadcrumbsFromUrl = (asPath: string, query:  { zoomLevel: ZoomLevel}) => {
+    let currentZoomLevel = 0
+    let pathArray = asPath.split('/')
+    pathArray = pathArray.filter(item => item !== '')
+    const startLandingIndex = pathArray.findIndex(item => item === 'landingpage')
+    const { zoomLevel } = query 
+    if (zoomLevel) {
+        currentZoomLevel = zoomLevel === ZoomLevel.Zoom1 ? 1 : 0
+    }
 
-    const level1 = {
-        name: '',
+    const level0 = {
+        name: 'The Other Interface',
         link: {pathname: '/'}
     }
 
-    const level2 = {
-        name: '',
+    const level1 = {
+        name: 'The Collection',
         link: {pathname: '/',  query: { zoomLevel: ZoomLevel.Zoom1 }}
     }
 
-    const level3 = getLevel3('')
 
+    if (pathArray.includes('landingpage')) {
+        currentZoomLevel = pathArray.length - startLandingIndex
+    }
+
+    if (pathArray.includes('story')) {
+        currentZoomLevel = 5
+    }
+
+    const rawItems = [level0, level1, getLevel2or3(pathArray[startLandingIndex + 1]), getLevel2or3(pathArray[startLandingIndex + 1], pathArray[startLandingIndex + 2]), getLevel4(pathArray[startLandingIndex + 1], pathArray[startLandingIndex + 2], pathArray[startLandingIndex + 3])]
+    const items = rawItems.slice(0, currentZoomLevel)
     return {
-        level1,
-        level2,
-        level3,
-        zoomLevel,
-        pathArray,
-        query
+        items
     }
 }
 
 const Breadcrumbs = () => {
     const router = useRouter()
-    getBreadcrumbsFromUrl(router.asPath)
+    getBreadcrumbsFromUrl(router.asPath, router?.query as { zoomLevel: ZoomLevel})
 
     return null
 }
