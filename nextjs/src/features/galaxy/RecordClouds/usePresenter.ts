95b8f2943c156200c5d3e939a1f26e304ab03ada
rick.woltheus@lifely.nl <rick.woltheus@lifely.nl>
2022-10-21 13:35:45 +0200

    *

--- nextjs/src/features/galaxy/RecordClouds/usePresenter.ts
+++ nextjs/src/features/galaxy/RecordClouds/usePresenter.ts
@@ -1,18 +1,55 @@
 import { useMemo, useRef } from 'react'
-import { ObjectRelationsQuery } from 'src/generated/graphql'
+import { EntityNames, ObjectRelationsQuery } from 'src/generated/graphql'
 
 const configuration = {
-    peope: {},
-    publications: {},
-    archives: {},
-    objects: {},
-    stories: {},
+    [EntityNames.Archives]: {},
+    [EntityNames.External]: {},
+    [EntityNames.Media]: {},
+    [EntityNames.Objects]: {},
+    [EntityNames.People]: {},
+    [EntityNames.Publications]: {},
+    [EntityNames.Stories]: {},
 }
+
+interface ParentRelation {
+    label: string
+    type: EntityNames
+    x: number
+    y: number
+    children: Relation[]
+}
+interface Relation {
+    label: string
+    type: EntityNames
+    position: number
+}
+
 export function usePresenter(relations: ObjectRelationsQuery['relations']) {
     const svgRef = useRef()
 
-    const relationsPositionData = useMemo(() => {
-        const test = relations?.map()
+    const relationsPositionData: ParentRelation[] = useMemo(() => {
+        const test =
+            relations?.map(relation => {
+                const config = configuration[relation.type]
+
+                return {
+                    label: relation.type,
+                    type: relation.type,
+                    x: 0,
+                    y: 0,
+                    children:
+                        relation.randomRelations?.map(randomRelation => {
+                            return {
+                                label: randomRelation.label,
+                                type: randomRelation.type,
+                                position: 100,
+                            }
+                        }) ?? [],
+                }
+            }) ?? []
+
+        return test
     }, [relations])
-    return { svgRef }
+
+    return { svgRef, relationsPositionData }
 }
