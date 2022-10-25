121c70cc8251cbb2594b00e629bddcb31cedb249
koenwisse <koen@launchme.eu>
2022-10-25 13:51:55 +0200

    fix(galaxy): cleanup

--- nextjs/src/features/shared/components/GalaxyFooter/GalaxyFooter.tsx
+++
@@ -1,8 +0,0 @@
-import { Box, Text } from '@chakra-ui/react'
-import { Markdown } from '../Markdown/Markdown'
-
-interface Props {}
-
-export const GalaxyFooter: React.FC<Props> = () => {
-    return <Box></Box>
-}
