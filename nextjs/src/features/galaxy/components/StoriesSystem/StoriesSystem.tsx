487f432019bbc113c2ebcf1ca6bcdfc7a02841c0
Gorkem Kockaya <gorkem.kockaya@gmail.com>
2022-10-28 17:51:15 +0300

    chore(stories-system): change to reduce diff

--- nextjs/src/features/galaxy/components/StoriesSystem/StoriesSystem.tsx
+++ nextjs/src/features/galaxy/components/StoriesSystem/StoriesSystem.tsx
@@ -35,30 +35,28 @@ export const StoriesSystem: React.FC<Props> = ({ data = [], dimensions = default
                     <polygon key={`${index}-${array.length}`} points={triangle.join()} fill={'transparent'}></polygon>
                 ))}
             </g>
-            {dataPoints.map((item, index, array) => {
-                return (
-                    <Link href={`/story/${item.slug}`} key={`${index}-${array.length}`}>
-                        <g className="StoriesSystem-dot" id={item.id}>
-                            <foreignObject
-                                x={`${item.point[0]}`}
-                                y={`${item.point[1]}`}
-                                height={'7.47px'}
-                                width={'7.47px'}
-                                className={`${item.parent}-dot`}
-                                data-id={item.id}
-                            >
-                                <Box
-                                    onMouseEnter={e => showTooltip(e, item)}
-                                    onMouseLeave={() => hideTooltip(item)}
-                                    height="100%"
-                                    width="100%"
-                                    background="radial-gradient(50% 50% at 50% 50%, #FFFFFF 0%, rgba(255, 255, 255, 0) 77.6%);"
-                                ></Box>
-                            </foreignObject>
-                        </g>
-                    </Link>
-                )
-            })}
+            {dataPoints.map((item, index, array) => (
+                <Link href={`/story/${item.slug}`} key={`${index}-${array.length}`}>
+                    <g className="StoriesSystem-dot" id={item.id}>
+                        <foreignObject
+                            x={`${item.point[0]}`}
+                            y={`${item.point[1]}`}
+                            height={'7.47px'}
+                            width={'7.47px'}
+                            className={`${item.parent}-dot`}
+                            data-id={item.id}
+                        >
+                            <Box
+                                onMouseEnter={e => showTooltip(e, item)}
+                                onMouseLeave={() => hideTooltip(item)}
+                                height="100%"
+                                width="100%"
+                                background="radial-gradient(50% 50% at 50% 50%, #FFFFFF 0%, rgba(255, 255, 255, 0) 77.6%);"
+                            ></Box>
+                        </foreignObject>
+                    </g>
+                </Link>
+            ))}
         </svg>
     )
 }
