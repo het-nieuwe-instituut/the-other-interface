1480fcdce8372a6de3b348a480c9e4339e96e8ca
Gorkem Kockaya <gorkem.kockaya@gmail.com>
2022-10-28 17:48:53 +0300

    feat: show description & fade

--- nextjs/src/features/galaxy/components/StoriesSystem/usePresenter.ts
+++ nextjs/src/features/galaxy/components/StoriesSystem/usePresenter.ts
@@ -9,6 +9,7 @@ import { calcRandomTrianglePoint, selectRandomTriangle } from '../../utils/polyg
 export interface InstancesPerClass {
     slug: string
     title: string
+    description?: string
     parent: string
     id: string
 }
@@ -61,6 +62,7 @@ interface DataPoint {
     point: number[]
     slug: string
     title: string
+    description?: string
     parent: string
     id: string
 }
@@ -184,37 +186,16 @@ function drawPathByParent(svgRef: null, dataPoints: InstancesPerClassWithPoint[]
 
 function showTooltip(e: React.MouseEvent<HTMLDivElement, MouseEvent>, item: DataPoint) {
     const storyEl = document.getElementById(item.id)
-
     if (!storyEl) {
         return
     }
 
     insertTooltip(item)
-
-    const tooltipContainerEl = getTooltipElement(item.id)
-    const tooltipEl = tooltipContainerEl?.children[0]
-
-    if (!tooltipEl) {
-        return
-    }
-
-    const tooltipRect = tooltipEl.getBoundingClientRect()
-
-    const left = e.clientX + 5
-    const top = e.clientY - tooltipRect.height - 5
-
-    tooltipContainerEl.style.left = `${left}px`
-    tooltipContainerEl.style.top = `${top}px`
-    tooltipContainerEl.style.visibility = 'visible'
+    positionAndShowTooltip(e, item)
 }
 
 function hideTooltip(item: DataPoint) {
-    let g = getTooltipElement(item.id)
-
-    while (g) {
-        g.remove()
-        g = getTooltipElement(item.id)
-    }
+    getTooltipElement(item.id)?.remove()
 }
 
 function insertTooltip(item: DataPoint) {
@@ -235,9 +216,10 @@ function insertTooltip(item: DataPoint) {
         background-color: rgba(255, 255, 255, 0.85);
         padding: 15px;
         border-radius: 5px;
+        overflow: hidden;
     `
 
-    const textStyle = `
+    const titleStyle = `
         font-family: 'Univers';
         font-style: normal;
         font-weight: 900;
@@ -247,17 +229,75 @@ function insertTooltip(item: DataPoint) {
         overflow: hidden;
         text-overflow: ellipsis;
         display: -webkit-box;
-        -webkit-line-clamp: 4;
-        line-clamp: 4;
+        -webkit-line-clamp: 2;
+        line-clamp: 2;
         -webkit-box-orient: vertical;
+    `
+
+    const descriptionStyle = `
+        .description,
+        .description-blurred {
+            margin-top: 13px;
+            font-family: 'Univers';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 12px;
+            line-height: 135%;
+            letter-spacing: -0.01em;
+            color: #001223;
+        }
+        .description-blurred:after {
+            content: "";
+            position: absolute;
+            z-index: 101;
+            bottom: 0;
+            left: 0;
+            border-radius: 5px;
+            pointer-events: none;
+            width: 100%;
+            height: 40px;
+            background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.95) 73.44%);
+            border-radius: 5px;
+        }
     `
 
     const tooltipDiv = document.createElement('div')
     tooltipDiv.setAttribute('id', `${item.id}-tooltip`)
     tooltipDiv.setAttribute('style', tooltipContainerStyle)
-    tooltipDiv.innerHTML += `<div style="${tooltipStyle}"><p style="${textStyle}">${item.title}</p></div>`
+    tooltipDiv.innerHTML += `
+        <div style="${tooltipStyle}">
+            <style>${descriptionStyle}</style>
+            <p style="${titleStyle}">${item.title}</p>
+            ${item.description ? `<p class="description-blurred">${item.description}</p>` : ''}
+        </div>
+    `
 
     document.body.insertBefore(tooltipDiv, null)
+}
+
+function positionAndShowTooltip(e: React.MouseEvent<HTMLDivElement, MouseEvent>, item: DataPoint) {
+    const tooltipContainerEl = getTooltipElement(item.id)
+    const tooltipEl = tooltipContainerEl?.children[0]
+    if (!tooltipEl) {
+        return
+    }
+
+    const bodyRect = document.body.getBoundingClientRect()
+    const tooltipRect = tooltipEl.getBoundingClientRect()
+
+    const descriptionEl = tooltipEl.querySelector('.description-blurred')
+    if (descriptionEl && tooltipEl.getBoundingClientRect().height < 325) {
+        descriptionEl.classList.replace('description-blurred', 'description')
+    }
+
+    const left = e.clientX + 5
+    const top = e.clientY - tooltipRect.height - 5
+    const adjustedLeft = left + tooltipRect.width > bodyRect.width ? bodyRect.width - tooltipRect.width : left
+    const adjustedTop = top < 0 ? tooltipRect.height : top
+
+    tooltipContainerEl.style.left = `${adjustedLeft}px`
+    tooltipContainerEl.style.top = `${adjustedTop}px`
+    tooltipContainerEl.style.visibility = 'visible'
 }
 
 function getTooltipElement(itemId: string) {
