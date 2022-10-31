5aca2c97c08206740852c3dfb4fb17f31c4c3f64
Nadine <nadine@lifely.nl>
2022-10-31 14:57:37 +0100

    Even better fix from reading more documentation

--- nextjs/src/features/shared/components/Markdown/Markdown.tsx
+++ nextjs/src/features/shared/components/Markdown/Markdown.tsx
@@ -4,25 +4,9 @@ import remarkGfm from 'remark-gfm'
 import rehypeRaw from 'rehype-raw'
 import DOMpurify from 'isomorphic-dompurify'
 import style from './markdown-styles.module.css'
-import { ReactNode } from 'react'
 
 interface Props {
     children: string
-}
-
-interface LinkProps {
-    href?: string
-    children?: ReactNode
-}
-
-const hyperlinks = {
-    a: (props: LinkProps) => {
-        return (
-            <a href={props.href} target="_blank" rel="noreferrer">
-                {props.children}
-            </a>
-        )
-    },
 }
 
 export const Markdown: React.FC<Props> = props => {
@@ -30,9 +14,10 @@ export const Markdown: React.FC<Props> = props => {
     return (
         <ReactMarkdown
             className={style.reactMarkDown}
-            components={ChakraUIRenderer(hyperlinks)}
+            components={ChakraUIRenderer()}
             remarkPlugins={[remarkGfm]}
             rehypePlugins={[rehypeRaw]}
+            linkTarget="_blank"
         >
             {DOMpurify.sanitize(props.children)}
         </ReactMarkdown>
