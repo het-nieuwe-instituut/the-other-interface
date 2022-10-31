e0b0ae771693f1cabc3bda38c6f7d4704f5f739d
Nadine <nadine@lifely.nl>
2022-10-31 14:39:05 +0100

    HNIT-709 Make hyperlinks open in new tab

--- nextjs/src/features/shared/components/Markdown/Markdown.tsx
+++ nextjs/src/features/shared/components/Markdown/Markdown.tsx
@@ -4,16 +4,30 @@ import remarkGfm from 'remark-gfm'
 import rehypeRaw from 'rehype-raw'
 import DOMpurify from 'isomorphic-dompurify'
 import style from './markdown-styles.module.css'
+import { ReactNode } from 'react'
 
 interface Props {
     children: string
+}
+
+interface LinkProps {
+    href?: string
+    children?: ReactNode
+}
+
+function LinkRenderer(props: LinkProps) {
+    return (
+        <a href={props.href} target="_blank" rel="noreferrer">
+            {props.children}
+        </a>
+    )
 }
 
 export const Markdown: React.FC<Props> = props => {
     return (
         <ReactMarkdown
             className={style.reactMarkDown}
-            components={ChakraUIRenderer()}
+            components={{ ...ChakraUIRenderer(), a: LinkRenderer }}
             remarkPlugins={[remarkGfm]}
             rehypePlugins={[rehypeRaw]}
         >
