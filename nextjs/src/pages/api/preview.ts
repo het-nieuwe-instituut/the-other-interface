614b7538197a22f5d7666f8d0764d04525b73eea
Yulia <yulia@info.nl>
2023-07-20 16:59:54 +0200

    feat: replaces ts patterns with switch statement

--- nextjs/src/pages/api/preview.ts
+++ nextjs/src/pages/api/preview.ts
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { match } from 'ts-pattern'
 import { z } from 'zod'
 
 type Data = {
@@ -33,49 +32,34 @@ const MenupagePreviewinputSchema = z.object({
 })
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const queryParams = QueryParams.parse(req.query)
-  const parsedData = JSON.parse(queryParams.data) as unknown
+  const { collectionTypeSlug, secret, data } = QueryParams.parse(req.query)
+  const parsedData = JSON.parse(data) as unknown
 
-  if (queryParams.secret !== process.env.STRAPI_PREVIEW_SECRET) {
+  if (secret !== process.env.STRAPI_PREVIEW_SECRET) {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
-  const url = match(queryParams)
-    .with(
-      { collectionTypeSlug: supportedCollectionType.Enum['api::landingpage.landingpage'] },
-      () => {
-        const data = LandingPagePreviewInputSchema.parse(parsedData)
-        return `/landingpage/${data.slug}`
-      }
-    )
-    .with(
-      {
-        collectionTypeSlug: supportedCollectionType.Enum['api::story.story'],
-      },
-      () => {
-        const data = StoryPreviewinputSchema.parse(parsedData)
-        return `/story/${data.id}-${data.slug}`
-      }
-    )
-    .with(
-      {
-        collectionTypeSlug: supportedCollectionType.Enum['api::homepage.homepage'],
-      },
-      () => {
-        return `/`
-      }
-    )
-    .with(
-      {
-        collectionTypeSlug: supportedCollectionType.Enum['api::menupage.menupage'],
-      },
-      () => {
-        const data = MenupagePreviewinputSchema.parse(parsedData)
-        return `/menupage/${data.slug}`
-      }
-    )
-    .exhaustive()
+  let url: string
 
+  switch (collectionTypeSlug) {
+    case supportedCollectionType.Enum['api::landingpage.landingpage']:
+      const landingPageData = LandingPagePreviewInputSchema.parse(parsedData)
+      url = `/landingpage/${landingPageData.slug}`
+      break
+    case supportedCollectionType.Enum['api::story.story']:
+      const storyData = StoryPreviewinputSchema.parse(parsedData)
+      url = `/story/${storyData.id}-${storyData.slug}`
+      break
+    case supportedCollectionType.Enum['api::homepage.homepage']:
+      url = `/`
+      break
+    case supportedCollectionType.Enum['api::menupage.menupage']:
+      const menuPageData = MenupagePreviewinputSchema.parse(parsedData)
+      url = `/menupage/${menuPageData.slug}`
+      break
+    default:
+      throw new Error('Unmatched collectionTypeSlug')
+  }
   if (!url) {
     return res.status(401).json({ message: `Preview route is not supported` })
   }
