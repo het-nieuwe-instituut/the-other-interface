f8c33827336832242ae5ed21363aeff7dd42ba48
Mykyta Siryk <countrytramp@gmail.com>
2023-07-25 11:34:59 +0200

    working middlaware example

--- strapi/src/api/story/middlewares/populate-triply.ts
+++ strapi/src/api/story/middlewares/populate-triply.ts
@@ -1,32 +1,22 @@
+const storyApi = 'api::story.story'
+
 export default (options, { strapi }) => {
-  return async (ctx, next, third) => {
-    console.log(third)
-    console.log({
-      options: options,
-    })
+  return async (ctx, next) => {
     await next()
 
-    const story = await strapi.entityService.findOne(
-      'api::story.story',
-      ctx.response.body.localizations[0].id,
-      {
+    if (ctx.request.body.model === storyApi) {
+      console.log('after')
+      const story = await strapi.entityService.findOne(storyApi, ctx.request.body.id, {
         populate: ['triplyRecords'],
+      })
+
+      ctx.response.body = {
+        ...ctx.response.body,
+        nonLocalizedFields: {
+          ...ctx.response.body.nonLocalizedFields,
+          triplyRecords: story.triplyRecords,
+        },
       }
-    )
-
-    console.log(story)
-
-    // console.log(ctx.request.header)
-    // console.log(ctx.request.params)
-    console.log(ctx.response.body)
-    // We can populate here notLocalizedFields with triplyRecords, and when we add it it will be always awailable
-    ctx.response.body = {
-      ...ctx.response.body,
-      nonLocalizedFields: {
-        ...ctx.response.body.nonLocalizedFields,
-        triplyRecords: story.triplyRecords,
-      },
     }
-    strapi.log.info('here')
   }
 }
