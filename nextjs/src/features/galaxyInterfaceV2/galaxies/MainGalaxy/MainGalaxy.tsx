70f49000e1c2cb1051eb77fe8f9ac04a8ce9074e
Yulia <yulia@info.nl>
2023-08-04 13:28:20 +0200

    refactor: code

--- nextjs/src/features/galaxyInterfaceV2/galaxies/MainGalaxy/MainGalaxy.tsx
+++ nextjs/src/features/galaxyInterfaceV2/galaxies/MainGalaxy/MainGalaxy.tsx
@@ -2,44 +2,42 @@ import { Box } from '@chakra-ui/react'
 
 import { CloudTitle, Cloud } from './fragments'
 
+const collectionClouds = [
+  {
+    title: 'Archives',
+    size: '144.4vw',
+    titlePosition: { left: '4.2vw', bottom: '42.5vh' },
+    cloudPosition: { left: 'calc(-144.4vw + 32vw)', bottom: 'calc(-144.4vw / 2 + 34vh)' },
+  },
+  {
+    title: 'Publications',
+    size: '135.4vw',
+    titlePosition: { right: '14vw', bottom: '26vh' },
+    cloudPosition: { right: 'calc(-135.4vw / 2 + 13vw)', bottom: 'calc(-135.4vw + 44vh)' },
+  },
+  {
+    title: 'Objects',
+    size: '84.41vw',
+    titlePosition: { right: '5.4vw', top: '22vh' },
+    cloudPosition: { top: 'calc(-84.41vw / 2 + 12.7vh)', right: 'calc(-84.41vw + 22vw)' },
+  },
+  {
+    title: 'People',
+    size: '62.42vw',
+    titlePosition: { left: '41vw', top: '6.4vh' },
+    cloudPosition: { top: 'calc(-62.42vw + 23.7vh)', left: 'calc(-62.42vw / 2 + 44vw)' },
+  },
+]
+
 const MainGalaxy: React.FC = () => {
   return (
     <Box position="relative" width="100vw" height="100vh">
-      <Cloud
-        size="144.4vw"
-        position={{
-          left: 'calc(-144.4vw + 32vw)',
-          bottom: 'calc(-144.4vw / 2 + 34vh)',
-        }}
-      />
-      <CloudTitle position={{ left: '4.2vw', bottom: '42.5vh' }}>Archives</CloudTitle>
-
-      <Cloud
-        size="135.4vw"
-        position={{
-          right: 'calc(-135.4vw / 2 + 13vw)',
-          bottom: 'calc(-135.4vw + 44vh)',
-        }}
-      />
-      <CloudTitle position={{ right: '14vw', bottom: '26vh' }}>Publications</CloudTitle>
-
-      <Cloud
-        size="84.41vw"
-        position={{
-          top: 'calc(-84.41vw / 2 + 12.7vh)',
-          right: 'calc(-84.41vw + 22vw)',
-        }}
-      />
-      <CloudTitle position={{ right: '5.4vw', top: '22vh' }}>Objects</CloudTitle>
-
-      <Cloud
-        size="62.42vw"
-        position={{
-          top: 'calc(-62.42vw + 23.7vh)',
-          left: 'calc(-62.42vw / 2 + 44vw)',
-        }}
-      />
-      <CloudTitle position={{ left: '41vw', top: '6.4vh' }}>People</CloudTitle>
+      {collectionClouds.map(cloud => (
+        <>
+          <Cloud size={cloud.size} position={cloud.cloudPosition} />
+          <CloudTitle position={cloud.titlePosition}>{cloud.title}</CloudTitle>
+        </>
+      ))}
     </Box>
   )
 }
