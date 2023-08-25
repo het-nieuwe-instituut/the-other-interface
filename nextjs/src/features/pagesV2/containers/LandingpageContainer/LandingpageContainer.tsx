0d09b950de2c86ea08688e3819df2b2f2a7c9a7a
Yulia <yulia@info.nl>
2023-08-25 14:15:31 +0200

    feat: editorial layer on landing page

---
+++ nextjs/src/features/pagesV2/containers/LandingpageContainer/LandingpageContainer.tsx
@@ -0,0 +1,29 @@
+import { GalaxyInterface } from '@/features/galaxyInterfaceV2/GalaxyInterface/GalaxyInterface'
+import dynamic from 'next/dynamic'
+import { Box } from '@chakra-ui/react'
+
+import { LandingpageBySlugQuery } from 'src/generated/graphql'
+import { EditorialLayer } from './fragments'
+
+export const DynamicMainGalaxyNoSsr = dynamic(
+  () => import('../../../galaxyInterfaceV2/galaxies/MainGalaxy/MainGalaxy'),
+  {
+    ssr: false,
+  }
+)
+
+interface Props {
+  landingpage?: LandingpageBySlugQuery
+}
+
+export const LandingpageContainer: React.FC<Props> = ({ landingpage }) => {
+  return (
+    <Box backgroundColor="graph">
+      <GalaxyInterface>
+        <DynamicMainGalaxyNoSsr />
+      </GalaxyInterface>
+
+      <EditorialLayer landingpage={landingpage} />
+    </Box>
+  )
+}
--- nextjs/src/pages/v2/landingpage.tsx
+++ nextjs/src/pages/v2/landingpage.tsx
@@ -1,45 +1,41 @@
 import { TypeToEntityName } from '@/features/galaxy/FilterClouds/FilterCloudsContainer'
 import { SupportedLandingPages } from '@/features/galaxy/PaginatedFilterClouds/PaginatedFilterCloudsContainer'
+import { LandingpageContainer } from '@/features/pagesV2/containers/LandingpageContainer/LandingpageContainer'
 
 import { initApiClient } from '@/features/shared/utils/api'
+import { Locale } from '@/features/shared/utils/locale'
 import { getPublicationState } from '@/features/shared/utils/publication-state'
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
 
 export interface LandingPageQueryParams {
   type: SupportedLandingPages
+  locale: Locale
 }
 
 const LandingPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   console.log('props', props)
-  return <div></div>
+
+  return <LandingpageContainer landingpage={props.landingpage} />
 }
 
 export default LandingPage
 
 export const getServerSideProps = async (context: GetServerSidePropsContext) => {
-  const { type } = context.query as unknown as LandingPageQueryParams
+  const { type, locale = 'en' } = context.query as unknown as LandingPageQueryParams
   const publicationState = getPublicationState(context.preview)
-  const entityName = TypeToEntityName[type]
-  let zoomLevel2
 
   const api = initApiClient(context)
 
-  if (entityName) {
-    const api = initApiClient(context)
-    zoomLevel2 = await api.zoomLevel2({ entityName })
-  }
-
   const landingpage = await api.landingpageBySlug({
-    slug: entityName,
-    locale: context?.locale,
+    slug: TypeToEntityName[type],
+    locale,
     publicationState,
   })
 
   return {
     props: {
-      type: entityName,
+      type,
       publicationState,
-      zoomLevel2,
       landingpage,
     },
   }
