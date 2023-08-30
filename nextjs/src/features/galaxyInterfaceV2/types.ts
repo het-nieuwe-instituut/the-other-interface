2e4046f42191eb0935e2e878e0e8e7390a5aae97
Yulia <yulia@info.nl>
2023-08-30 14:43:09 +0200

    refactor: remove excessive

--- nextjs/src/features/galaxyInterfaceV2/types.ts
+++
@@ -1,9 +0,0 @@
-import { EntityNames } from 'src/generated/graphql'
-import { CloudCategory } from '../shared/types/categories'
-
-export const CategoryToEntityName: Record<CloudCategory, EntityNames> = {
-  publications: EntityNames.Publications,
-  people: EntityNames.People,
-  objects: EntityNames.Objects,
-  archives: EntityNames.Archives,
-}
