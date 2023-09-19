4a4f1e35a6944709b121ad70bac2f65bf91fce0e
Yulia <yulia@info.nl>
2023-09-19 10:47:50 +0200

    fix: position type

--- nextjs/src/features/shared/types/position.ts
+++ nextjs/src/features/shared/types/position.ts
@@ -1,4 +1,4 @@
-type PositionValue = `${string}vw` | `${string}vh` | `${string}%` | 0
+type PositionValue = `${string}vw` | `${string}vh` | `${string}%` | 0 | `calc(${string})`
 
 export type Position = {
   top?: PositionValue
