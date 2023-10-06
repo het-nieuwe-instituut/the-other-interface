8bf3e17d4c569b011a9a4a012a01896e36525c42
Yulia <yulia@info.nl>
2023-10-06 13:53:12 +0200

    feat: suggestions

--- nextjs/src/features/galaxyInterface/components/GalaxyWrapper/GalaxySearchBar/SuggestionBar/SuggestionBar.tsx
+++ nextjs/src/features/galaxyInterface/components/GalaxyWrapper/GalaxySearchBar/SuggestionBar/SuggestionBar.tsx
@@ -1,18 +1,23 @@
+import { SearchFilterBox } from '@/features/shared/components/SearchFilterBox/SearchFilterBox'
 import { FOOTER_Z_INDEX } from '@/features/shared/constants/mainConstants'
-import { Flex } from '@chakra-ui/react'
+import { useTypeSafeTranslation } from '@/features/shared/hooks/translations'
+import { CLOUD_CATEGORIES } from '@/features/shared/utils/categories'
+import { Flex, Box, Grid } from '@chakra-ui/react'
 
 interface Props {
   isOpen?: boolean
 }
 
 export const SuggestionBar: React.FC<Props> = ({ isOpen = false }) => {
+  const { t } = useTypeSafeTranslation('category')
+
   return (
-    <Flex
+    <Grid
       position={'absolute'}
       bottom={'60px'}
       height="75px"
       width={'100%'}
-      gap="5px"
+      gap="20px"
       borderRadius={'5px 5px 0 0'}
       backgroundColor={'blueAlpha.80'}
       padding={'5px 20px'}
@@ -23,6 +28,21 @@ export const SuggestionBar: React.FC<Props> = ({ isOpen = false }) => {
       }
       opacity={isOpen ? 1 : 0}
       visibility={isOpen ? 'visible' : 'hidden'}
-    ></Flex>
+      border="1px solid red"
+      templateColumns={'146px 1fr'}
+    >
+      <Box width="146px" border="1px solid red"></Box>
+      <Box overflow="hidden" width="100%">
+        <Flex gap="5px" width={'100%'} overflowX="auto" paddingBottom="20px" marginBottom="-20px">
+          {[...Object.values(CLOUD_CATEGORIES), ...Object.values(CLOUD_CATEGORIES)].map(
+            category => (
+              <Box key={category}>
+                <SearchFilterBox category={t('category')} subCategory={t(category)} />
+              </Box>
+            )
+          )}
+        </Flex>
+      </Box>
+    </Grid>
   )
 }
