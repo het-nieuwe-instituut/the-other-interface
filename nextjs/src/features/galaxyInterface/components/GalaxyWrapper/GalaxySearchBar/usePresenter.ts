1ad6c79135693c3bac7b87cc8102660fb0242f35
Yulia <yulia@info.nl>
2023-10-10 13:18:52 +0200

    feat: usePageCategoryHook

--- nextjs/src/features/galaxyInterface/components/GalaxyWrapper/GalaxySearchBar/usePresenter.ts
+++ nextjs/src/features/galaxyInterface/components/GalaxyWrapper/GalaxySearchBar/usePresenter.ts
@@ -1,17 +1,15 @@
 import { State } from '@/features/shared/configs/store'
 import { getCurrentZoomNumber } from '@/features/shared/helpers/getCurrentZoomNumber'
 import { useTypeSafeTranslation } from '@/features/shared/hooks/translations'
+import { usePageCategory } from '@/features/shared/hooks/usePageCategory'
 import { sharedActions } from '@/features/shared/stores/shared.store'
-import { CATEGORIES, Category } from '@/features/shared/utils/categories'
-import { useParams, usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { useEffect, useMemo } from 'react'
+import { usePathname, useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 export const usePresenter = () => {
   const router = useRouter()
   const pathname = usePathname()
-  const params = useParams()
-  const searchParams = useSearchParams()
   const dispatch = useDispatch()
   const isSearchModeActive = useSelector((state: State) => state.shared.isSearchModeActive)
   const searchCategory = useSelector((state: State) => state.shared.searchCategory)
@@ -20,32 +18,15 @@ export const usePresenter = () => {
   )
   const { t } = useTypeSafeTranslation('category')
 
-  const initialCategory = useMemo(() => {
-    const currentZoomNumber = getCurrentZoomNumber(pathname)
-    let category: Category | null = null
-
-    if (currentZoomNumber === 2) {
-      category = searchParams?.get('category') as Category
-    }
-
-    if (currentZoomNumber === 3) {
-      category = params?.category as Category
-    }
-
-    if (category === CATEGORIES.stories) {
-      category = null
-    }
-
-    return category
-  }, [pathname, params, searchParams])
+  const { pageCategory } = usePageCategory()
 
   useEffect(() => {
-    dispatch(sharedActions.searchCategory({ searchCategory: initialCategory }))
+    dispatch(sharedActions.searchCategory({ searchCategory: pageCategory }))
 
     return () => {
       dispatch(sharedActions.searchModeActive({ isSearchModeActive: false }))
     }
-  }, [initialCategory, dispatch])
+  }, [pageCategory, dispatch])
 
   useEffect(() => {
     dispatch(sharedActions.searchModeActive({ isSearchModeActive: false }))
