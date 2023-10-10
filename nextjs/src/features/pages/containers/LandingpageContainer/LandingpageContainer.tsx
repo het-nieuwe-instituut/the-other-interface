f76306ca9955f4e88a9ba5b30ebeeb9b689de291
Yulia <yulia@info.nl>
2023-10-10 12:53:15 +0200

    feat: moved zoom2 galaxy data fetch to client side

--- nextjs/src/features/pages/containers/LandingpageContainer/LandingpageContainer.tsx
+++ nextjs/src/features/pages/containers/LandingpageContainer/LandingpageContainer.tsx
@@ -1,22 +1,21 @@
 import { GalaxyInterface } from '@/features/galaxyInterface/GalaxyInterface/GalaxyInterface'
 import { EditorialLayer } from '@/features/shared/components/EditorialLayer/EditorialLayer'
 import { CloudCategory } from '@/features/shared/utils/categories'
-import { LandingpageBySlugQuery, Zoom2Query } from 'src/generated/graphql'
+import { LandingpageBySlugQuery } from 'src/generated/graphql'
 import { CategoryGalaxy } from '../../../galaxyInterface/galaxies/CategoryGalaxy/CategoryGalaxy'
 import { Box } from '@chakra-ui/react'
 
 interface Props {
   category: CloudCategory
   landingpage?: LandingpageBySlugQuery
-  zoom2: Zoom2Query
 }
 
-export const LandingpageContainer: React.FC<Props> = ({ landingpage, zoom2 }) => {
+export const LandingpageContainer: React.FC<Props> = ({ landingpage }) => {
   const editorialData = landingpage?.landingpages?.data[0]?.attributes
   return (
     <Box backgroundColor="graph">
       <GalaxyInterface>
-        <CategoryGalaxy zoom2={zoom2} />
+        <CategoryGalaxy />
       </GalaxyInterface>
 
       {editorialData && (
