d48f28b550260d6685f926cce9eeb434c37021a3
Yulia <yulia@info.nl>
2023-11-01 14:03:21 +0400

    refactor: pagination

--- nextjs/src/features/shared/hooks/usePagination.ts
+++ nextjs/src/features/shared/hooks/usePagination.ts
@@ -4,14 +4,20 @@ import { ZOOM2_RECORDS_PER_PAGE } from '../constants/mainConstants'
 export const usePagination = (searchResultAmount: number) => {
   const pathname = usePathname()
   const router = useRouter()
-  const searchParams = useSearchParams() || new URLSearchParams()
+  const searchParams = useSearchParams()
 
-  const page = Number(searchParams.get('page') as string) || 1
+  const page = searchParams ? Number(searchParams.get('page') as string) : 1
   const pageAmount = searchResultAmount ? Math.ceil(searchResultAmount / ZOOM2_RECORDS_PER_PAGE) : 0
 
   const paginate = (pageNumber: number) => {
-    searchParams.set('page', String(pageNumber))
-    const url = `${pathname}?${searchParams.toString()}`
+    const updatedSearchParams = searchParams
+      ? new URLSearchParams(searchParams.toString())
+      : new URLSearchParams()
+
+    updatedSearchParams.set('page', String(pageNumber))
+
+    const url = pathname + `?${updatedSearchParams.toString()}`
+
     router.push(url)
   }
 
