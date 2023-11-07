416721f05c033c553a10b3a86c01f9f89c60d822
Yulia <yulia@info.nl>
2023-11-07 08:59:25 +0400

    feat: useRef feature

--- nextjs/src/features/shared/hooks/queries/useZoom2SearchResult.ts
+++ nextjs/src/features/shared/hooks/queries/useZoom2SearchResult.ts
@@ -3,7 +3,7 @@ import { useQuery, useQueryClient } from '@tanstack/react-query'
 import initApiClientService from '../../utils/initApiClientService'
 import { CATEGORIES_TO_ENTITY_MAPPER, CloudCategory } from '@/features/shared/utils/categories'
 import { ZOOM2_RECORDS_PER_PAGE } from '../../constants/mainConstants'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export function useZoom2SearchResult({
   category,
@@ -18,19 +18,40 @@ export function useZoom2SearchResult({
   const queryClient = useQueryClient()
 
   const [shouldFetch, setShouldFetch] = useState(false)
+  const isCurrentPage = useRef(true)
 
   const entityName = CATEGORIES_TO_ENTITY_MAPPER[category as CloudCategory]
 
   useEffect(() => {
+    // Set shouldFetch to false when page changes
     setShouldFetch(false)
-    console.log(`${page}, useEffect, shouldFetch: ${shouldFetch}}`)
-    // Fetch data only if user is at least 300 ms on a page
+
+    // Update the ref to indicate the current page
+    isCurrentPage.current = true
+
+    console.log(
+      `${page}, useEffect, shouldFetch: ${shouldFetch}}, isCurrentPage: ${isCurrentPage.current}`
+    )
+    // Start a timeout to set shouldFetch to true
     const handle = setTimeout(() => {
-      console.log(`${page} setTimeout, shouldFetch: ${shouldFetch}`)
-      setShouldFetch(true)
+      if (isCurrentPage.current) {
+        console.log(
+          `${page}, setTimeout, shouldFetch: ${shouldFetch}}, isCurrentPage: ${isCurrentPage.current}`
+        )
+        setShouldFetch(true)
+      }
     }, 500)
 
-    return () => clearTimeout(handle)
+    // Cleanup function to run when the component unmounts or the page changes
+    return () => {
+      // Clear the timeout
+      clearTimeout(handle)
+      // Update the ref to indicate that we've moved on from this page
+      isCurrentPage.current = false
+      console.log(
+        `${page}, return, shouldFetch: ${shouldFetch}}, isCurrentPage: ${isCurrentPage.current}`
+      )
+    }
   }, [page])
 
   return useQuery({
