931947b8c7e0555eae3cd942e59762ebd9ac41b4
Mykyta Siryk <countrytramp@gmail.com>
2023-11-08 15:35:49 +0100

    save progress, change greed

--- nextjs/src/features/shared/components/Footer/Footer.tsx
+++ nextjs/src/features/shared/components/Footer/Footer.tsx
@@ -5,6 +5,7 @@ import { FOOTER_Z_INDEX } from '../../constants/mainConstants'
 import { capitalizeFirstLetter } from '../../utils/text'
 import { MenuPagesQuery } from 'src/generated/graphql'
 import { usePresenter } from './usePresenter'
+import Logo from '@/icons/logo.svg'
 
 type Props = {
   menupages?: MenuPagesQuery
@@ -12,11 +13,20 @@ type Props = {
 
 export const Footer = ({ menupages }: Props) => {
   const { sectionOne, sectionTwo, lang } = usePresenter(menupages)
+  const hasSectionOne = sectionOne && sectionOne.length > 0
+  const hasSectionTwo = sectionTwo && sectionTwo.length > 0
+
+  // const hasSectionOne = sectionOne && sectionOne.length > 0
+  // const hasSectionTwo = sectionTwo && sectionTwo.length > 0
+
+  const dynamicSectionPresent = hasSectionOne || hasSectionTwo
 
   return (
     <Flex
       justifyContent={'space-between'}
-      minHeight={{ sm: '428px', md: '428px', lg: '233px', xl: '233px', '2xl': '233px' }}
+      direction={{ base: 'column', md: 'row', lg: 'row', xl: 'row' }}
+      alignItems={{ base: 'center', md: 'start', lg: 'start', xl: 'start' }}
+      minHeight={{ base: 'auto', md: '846px', lg: '493px', xl: '189px', '2xl': '189px' }}
       px={'8'}
       marginX={'auto'}
       paddingTop={'9'}
@@ -25,70 +35,75 @@ export const Footer = ({ menupages }: Props) => {
       position={'relative'}
       zIndex={FOOTER_Z_INDEX}
     >
+      {/* Logo and Static Links Column */}
       <Flex
-        flex={0.16}
-        position={'relative'}
-        justifyContent={{ sm: 'flex-start', md: 'flex-start', lg: 'center', xl: 'center' }}
+        direction="column"
+        flex={{ md: 1, lg: 1, xl: dynamicSectionPresent ? '0.3333' : '0.5' }}
+        alignItems={{ md: 'flex-start', lg: 'flex-start', xl: 'flex-start' }}
+        mb={{ base: 5, md: 0 }}
       >
-        <Text
-          textStyle="h3"
-          position={'absolute'}
-          top={0}
-          transform={'rotate(180deg)'}
-          css={{ writingMode: 'vertical-rl' }}
-          textAlign={'center'}
-        >
-          Het Nieuwe Instituut
-        </Text>
-      </Flex>
-
-      <Flex
-        justifyContent={'space-between'}
-        flex={0.76}
-        flexWrap={'wrap'}
-        flexDirection={{ sm: 'column', md: 'column', lg: 'unset', xl: 'row' }}
-      >
-        <Flex flex={0.3} flexDirection={'column'} justifyContent={'space-between'}>
-          <Link
-            mr={2}
-            href={`https://goo.gl/maps/m5EvkSa3z321G68z6`}
-            variant={'decorative'}
-            cursor="pointer"
-            textStyle="body"
-          >
+        <Logo />
+        <Flex direction="column" mt={{ base: 5, lg: 5, xl: 0 }} alignItems="flex-start">
+          {/* Insert static links here */}
+          <Link href={`https://goo.gl/maps/m5EvkSa3z321G68z6`} variant={'decorative'}>
             Het Nieuwe Instituut Museumpark 25 3015CB Rotterdam
           </Link>
-          <Flex flexDirection={'column'}>
-            <Link
-              mr={2}
-              href={`tel:+31(0)10-4401200`}
-              variant={'decorative'}
-              cursor="pointer"
-              textStyle="body"
-            >
-              +31(0)10-4401200
-            </Link>
-            <Link
-              href={`mailto:info@hetnieuweinstituut.net`}
-              variant={'decorative'}
-              cursor="pointer"
-              textStyle="body"
-            >
-              Info@hetnieuweinstituut.net
-            </Link>
-          </Flex>
+          <Link href={`tel:+31(0)10-4401200`} variant={'decorative'}>
+            +31(0)10-4401200
+          </Link>
+          <Link href={`mailto:info@hetnieuweinstituut.net`} variant={'decorative'}>
+            Info@hetnieuweinstituut.net
+          </Link>
         </Flex>
+        {/* <Link
+          href={`https://goo.gl/maps/m5EvkSa3z321G68z6`}
+          variant={'decorative'}
+          cursor="pointer"
+          textStyle="body"
+          mt={{ base: 5, lg: 5, xl: 0 }}
+          mr={{ xl: 2 }}
+        >
+          Het Nieuwe Instituut Museumpark 25 3015CB Rotterdam
+        </Link>
+        <Link
+          href={`tel:+31(0)10-4401200`}
+          variant={'decorative'}
+          cursor="pointer"
+          textStyle="body"
+          mt={{ base: 5, lg: 5, xl: 0 }}
+          mr={{ xl: 2 }}
+        >
+          +31(0)10-4401200
+        </Link>
+        <Link
+          href={`mailto:info@hetnieuweinstituut.net`}
+          variant={'decorative'}
+          cursor="pointer"
+          textStyle="body"
+          mt={{ base: 5, lg: 5, xl: 0 }}
+        >
+          Info@hetnieuweinstituut.net
+        </Link> */}
+      </Flex>
 
-        {sectionOne && sectionOne?.length > 0 && (
-          <Flex
-            flex={0.3}
-            flexDirection={'column'}
-            minW={{ sm: '400px', md: '400px', lg: 'unset', xl: 'unset' }}
-          >
-            <>
+      {dynamicSectionPresent && (
+        <Flex
+          direction={{ base: 'column', md: 'row' }}
+          flex={{ md: 1, lg: 1, xl: '0.6666' }}
+          justifyContent="space-between"
+          mb={{ base: 5, md: 0 }}
+          w={{ lg: 'auto', xl: '66.66%' }} // Ensure the dynamic columns don't shrink below content width on 'lg'
+        >
+          {/* Dynamic Links Column 1 */}
+          {hasSectionOne && (
+            <Flex
+              direction="column"
+              flex={{ xl: '0.3333' }}
+              alignItems={{ lg: 'flex-start', xl: 'flex-start' }}
+            >
               {sectionOne.map(link => (
                 <Link
-                  key={Math.random()}
+                  key={link.id} // Use unique ID from your data
                   href={`${link?.attributes?.slug}`}
                   variant={'decorative'}
                   cursor="pointer"
@@ -97,20 +112,19 @@ export const Footer = ({ menupages }: Props) => {
                   {capitalizeFirstLetter(link?.attributes?.Title ?? '')}
                 </Link>
               ))}
-            </>
-          </Flex>
-        )}
+            </Flex>
+          )}
 
-        {sectionTwo && sectionTwo?.length > 0 && (
-          <Flex
-            flex={0.3}
-            flexDirection={'column'}
-            minW={{ sm: '400px', md: '400px', lg: 'unset', xl: 'unset' }}
-          >
-            <>
+          {/* Dynamic Links Column 2 */}
+          {hasSectionTwo && (
+            <Flex
+              direction="column"
+              flex={{ xl: '0.3333' }}
+              alignItems={{ lg: 'flex-start', xl: 'flex-start' }}
+            >
               {sectionTwo.map(link => (
                 <Link
-                  key={Math.random()}
+                  key={link.id} // Use unique ID from your data
                   href={`/menupage/${link?.attributes?.slug}?lang=${lang}`}
                   variant={'decorative'}
                   cursor="pointer"
@@ -119,10 +133,11 @@ export const Footer = ({ menupages }: Props) => {
                   {capitalizeFirstLetter(link?.attributes?.Title ?? '')}
                 </Link>
               ))}
-            </>
-          </Flex>
-        )}
-      </Flex>
+            </Flex>
+          )}
+        </Flex>
+      )}
+      {/* Dynamic Links Columns */}
     </Flex>
   )
 }
