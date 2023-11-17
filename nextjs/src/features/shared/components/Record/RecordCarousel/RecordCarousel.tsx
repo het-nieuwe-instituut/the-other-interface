6fa32ba934d01867af60552ae384f6464bf17f81
Mykyta Siryk <countrytramp@gmail.com>
2023-11-17 11:41:53 +0100

    fix: record fixes

--- nextjs/src/features/shared/components/Record/RecordCarousel/RecordCarousel.tsx
+++ nextjs/src/features/shared/components/Record/RecordCarousel/RecordCarousel.tsx
@@ -36,48 +36,44 @@ const RecordCarousel = (props: { imageUrls: string[] }) => {
       <Box height="523.605px" width="100%">
         <ResponsiveImage
           src={selectedImage ?? props.imageUrls[0]}
-          css={{ height: '100%' }}
+          css={{ height: '100%', objectFit: 'contain' }}
           maxHeight="523px"
-          fit="fill"
+          fit="contain"
         />
       </Box>
-      <Flex mt="30px" position={'relative'} width={'100%'}>
-        {displayedThumbnails.map(url => (
-          <Box key={url} mx="6px" height={'103.702px'} width="25%">
-            <ResponsiveImage
-              src={url}
-              css={{
-                height: '100%',
-                width: '100%',
-                transition: 'opacity 0.5s ease-out',
-                cursor: 'pointer',
-              }}
-              maxHeight="103.702px"
-              fit="fill"
-              onClick={() => handleThumbnailClick(url)}
-            />
+
+      {props.imageUrls.length > 1 && (
+        <Flex mt="30px" position={'relative'} width={'100%'}>
+          {displayedThumbnails.map(url => (
+            <Box key={url} mx="6px" height={'103.702px'} width="25%">
+              <ResponsiveImage
+                src={url}
+                css={{
+                  height: '100%',
+                  width: '100%',
+                  transition: 'opacity 0.5s ease-out',
+                  cursor: 'pointer',
+                }}
+                maxHeight="103.702px"
+                fit="contain"
+                onClick={() => handleThumbnailClick(url)}
+              />
+            </Box>
+          ))}
+          <Box
+            position="absolute"
+            right="0"
+            width="80px"
+            height={'100%'}
+            backgroundColor={'transparent'}
+            display={'flex'}
+            alignItems={'center'}
+          >
+            <RecordCarouselButton direction="left" onClick={handleBack} />
+            <RecordCarouselButton direction="right" onClick={handleNext} />
           </Box>
-        ))}
-        <Box
-          position="absolute"
-          right="80px"
-          width="46.211px"
-          height={'100%'}
-          background="linear-gradient(270deg, #FFF -0.01%, rgba(255, 255, 255, 0.00) 100%)"
-        />
-        <Box
-          position="absolute"
-          right="0"
-          width="80px"
-          height={'100%'}
-          backgroundColor={'ivoryAlpha.95'}
-          display={'flex'}
-          alignItems={'center'}
-        >
-          <RecordCarouselButton direction="left" onClick={handleBack} />
-          <RecordCarouselButton direction="right" onClick={handleNext} />
-        </Box>
-      </Flex>
+        </Flex>
+      )}
     </Flex>
   )
 }
