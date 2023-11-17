ee819c4e6c84d02aed8a5049a4fc742f8849c617
Yulia <yulia@info.nl>
2023-11-17 16:29:13 +0100

    feat: record header scroll by click

--- nextjs/src/features/shared/components/Record/RecordHeader/RecordHeader.tsx
+++ nextjs/src/features/shared/components/Record/RecordHeader/RecordHeader.tsx
@@ -4,12 +4,13 @@ import { RecordTitle } from '../fragments/RecordTitle'
 import { RecordDescription } from '../fragments/RecordDescription'
 import RecordCarousel from '../RecordCarousel/RecordCarousel'
 import { usePresenter } from './usePresenter'
+import { ScrollToContent } from '@/features/pages/utils/utils'
 
 export const RecordHeader = () => {
-  const { record } = usePresenter()
+  const { record, scrollPosition } = usePresenter()
 
   return (
-    <Box>
+    <Box onClick={ScrollToContent} cursor={scrollPosition < 750 ? 'pointer' : 'cursor'}>
       <RecordTitle title={record?.title ?? ''} />
       <RecordDescription description={record?.description ?? ''} />
       {record?.thumbnail && record?.thumbnail.length > 0 && (
