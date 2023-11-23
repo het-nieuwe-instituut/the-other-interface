7bb31fbe219c2647ea3aec2e1d218a542bbbd051
Yulia <yulia@info.nl>
2023-11-23 11:57:10 +0100

    feat: use layer wrapper for record and story

--- nextjs/src/features/shared/components/Layers/RecordLayer/RecordLayer.tsx
+++ nextjs/src/features/shared/components/Layers/RecordLayer/RecordLayer.tsx
@@ -1,30 +1,7 @@
 'use client'
-import { Box, Grid, GridItem, useTheme } from '@chakra-ui/react'
 import { RecordHeader } from '../../Record/RecordHeader/RecordHeader'
+import { LayerWrapper } from '../LayerWrapper'
 
 export const RecordLayer = () => {
-  const theme = useTheme()
-
-  return (
-    <Box position={'relative'} zIndex={2} backgroundColor={'ivoryAlpha.95'}>
-      <Box maxW={theme.breakpoints.xl} marginX={'auto'} px={6}>
-        <Grid
-          pt={9}
-          templateAreas={{
-            lg: `"header meta"`,
-            base: `"meta"
-            "header"`,
-          }}
-          templateColumns={{ lg: '1fr 22.438rem', base: `100% 100%` }}
-          templateRows={{ lg: '1fr', base: `auto minmax(0, 1fr)` }}
-          gap={'3.75rem'}
-        >
-          <GridItem area={'header'}>
-            <RecordHeader />
-          </GridItem>
-          <GridItem area={'meta'}></GridItem>
-        </Grid>
-      </Box>
-    </Box>
-  )
+  return <LayerWrapper LeftContent={<RecordHeader />} />
 }
--- nextjs/src/features/shared/components/Layers/StoryLayer/StoryLayer.tsx
+++ nextjs/src/features/shared/components/Layers/StoryLayer/StoryLayer.tsx
@@ -1,37 +1,22 @@
 'use client'
-import { Box, Grid, GridItem, useTheme } from '@chakra-ui/react'
 import { PageHeader } from '../../PageHeader/PageHeader'
 import { StoryMeta } from '../../Meta/StoryMeta/StoryMeta'
 import { usePresenter } from './usePresenter'
 import { DynamicComponentRenderer } from '@/features/modules/ModulesRenderer/ModulesRenderer'
+import { LayerWrapper } from '../LayerWrapper'
 
 export const StoryLayer = () => {
-  const theme = useTheme()
   const { story } = usePresenter()
 
   return (
-    <Box position={'relative'} zIndex={2} backgroundColor={'ivoryAlpha.95'}>
-      <Box maxW={theme.breakpoints.xl} marginX={'auto'} px={6}>
-        <Grid
-          pt={9}
-          templateAreas={{
-            lg: `"header meta"`,
-            base: `"meta"
-            "header"`,
-          }}
-          templateColumns={{ lg: '1fr 22.438rem', base: `100% 100%` }}
-          templateRows={{ lg: '1fr', base: `auto minmax(0, 1fr)` }}
-          gap={'3.75rem'}
-        >
-          <GridItem area={'header'}>
-            <PageHeader title={story?.title} preface={story?.description} />
-            <DynamicComponentRenderer components={story?.attributes?.components} />
-          </GridItem>
-          <GridItem area={'meta'}>
-            <StoryMeta />
-          </GridItem>
-        </Grid>
-      </Box>
-    </Box>
+    <LayerWrapper
+      LeftContent={
+        <>
+          <PageHeader title={story?.title} preface={story?.description} />
+          <DynamicComponentRenderer components={story?.attributes?.components} />
+        </>
+      }
+      RightContent={<StoryMeta />}
+    />
   )
 }
