5daea66a52a2774aa95c4d2a2454edfe5e7cd847
Mykyta Siryk <countrytramp@gmail.com>
2024-04-25 11:09:13 +0200

    fix counters

--- nextjs/src/features/galaxyInterface/galaxies/MainGalaxy/hooks/useTotalCounter.ts
+++ nextjs/src/features/galaxyInterface/galaxies/MainGalaxy/hooks/useTotalCounter.ts
@@ -3,11 +3,14 @@ import initApiClientService from '@/features/shared/utils/initApiClientService'
 import { useQuery } from '@tanstack/react-query'
 import { useSearchParams } from 'next/navigation'
 import { sumTotalCounts } from '../helpers/sumTotalCounts'
+import { useSelector } from 'react-redux'
+import { State } from '@/features/shared/configs/store'
 
 export function useTotalCounter(shouldFetch = false) {
   const api = initApiClientService()
   const searchParams = useSearchParams()
   const lang = searchParams?.get('lang') || 'nl'
+  const { searchCategory } = useSelector((state: State) => state.shared)
 
   const queryFn = async () => {
     if (!shouldFetch) {
@@ -16,11 +19,11 @@ export function useTotalCounter(shouldFetch = false) {
     const data = await api.totalCounter({
       locale: lang,
     })
-    return sumTotalCounts(data)
+    return sumTotalCounts(data, searchCategory)
   }
 
   return useQuery({
-    queryKey: ['total-counter', lang, shouldFetch],
+    queryKey: ['total-counter', lang, shouldFetch, searchCategory],
     queryFn,
     refetchOnWindowFocus: false,
     keepPreviousData: true,
