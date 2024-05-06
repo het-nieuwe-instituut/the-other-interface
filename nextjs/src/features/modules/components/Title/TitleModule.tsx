75e9ad3b864a92c9075a20a22fb0143db3722aaa
Rick Woltheus <rwoltheus@gmail.com>
2024-05-06 15:04:58 +0200

    *

--- nextjs/src/features/modules/components/Title/TitleModule.tsx
+++ nextjs/src/features/modules/components/Title/TitleModule.tsx
@@ -1,4 +1,7 @@
-import { ImpactLargeTypographyVariants } from '@/features/ui/typography/variants'
+import {
+  ImpactLargeTypographyVariants,
+  TypographyVariants,
+} from '@/features/ui/typography/variants'
 import { ComponentModulesTitleModule } from 'src/generated/graphql'
 
 interface Props {
@@ -8,7 +11,7 @@ interface Props {
 export const TitleModule: React.FC<Props> = props => {
   return (
     <div className="w-full bg-inherit pt-2 pb-2">
-      <h1 className={ImpactLargeTypographyVariants({ intent: 'xl' })}>{props.component.Title}</h1>
+      <h1 className={TypographyVariants({ impact: 'xl' })}>{props.component.Title}</h1>
     </div>
   )
 }
