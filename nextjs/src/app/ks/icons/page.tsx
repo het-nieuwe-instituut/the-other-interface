e00e1e98935322fe60b40a65e564221ecfe27879
Rick Woltheus <rwoltheus@gmail.com>
2024-05-10 16:10:18 +0200

    add icons to project

--- nextjs/src/app/ks/icons/page.tsx
+++ nextjs/src/app/ks/icons/page.tsx
@@ -1,26 +1,45 @@
-import fs from 'fs'
+// Import all SVG files from a directory
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const importAll = (r: any) => {
+  const icons = {}
+  r.keys().map((item: string) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ;(icons as any)[item.replace('./', '') as any] = r(item)
+  })
+  return icons
+}
 
-const iconsPath = '../../../src/app/ks/icons'
-const Page = async () => {
-  const paths = fs.readdirSync(iconsPath)
+// Import all SVG files from the directory
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const icons: Record<string, { default: any }> = importAll(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (require as any).context('@/features/ui/icons', false, /\.svg$/)
+)
 
-  const loadComponent = async (path: string) => {
-    const Icon = await import(`../../../../${path}`)
-    return <Icon />
-  }
+// List of icon names
+const iconNames = Object.keys(icons)
 
-  const components = await Promise.all(
-    paths.map(async path => {
-      return await loadComponent(`${iconsPath}/${path}`)
-    })
-  )
-
+const Page = () => {
+  const keys = Object.keys(icons)
   return (
-    <div className="flex flex-col gap-4">
+    <>
       <p>Icons</p>
+      example: <br /> <code>{`import IconName from '@/features/ui/icons/icon-name.svg'`}</code>
+      <br />
+      <hr />
+      <div className="grid grid-cols-4 gap-4">
+        {iconNames.map((icon, index) => {
+          const IconComponent = icons[icon].default
 
-      <div className="flex">{components}</div>
-    </div>
+          return (
+            <div key={index} className="">
+              <p>{keys[index].split('/').reverse()[0]}</p>
+              <IconComponent key={index} />{' '}
+            </div>
+          )
+        })}
+      </div>
+    </>
   )
 }
 
