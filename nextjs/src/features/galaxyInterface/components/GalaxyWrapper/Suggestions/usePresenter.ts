5509fb0a41e2e6b1a78f4b00f0dd864fe6aee831
Mykyta Siryk <countrytramp@gmail.com>
2024-05-15 14:13:19 +0200

    remove unused usePresenter

--- nextjs/src/features/galaxyInterface/components/GalaxyWrapper/Suggestions/usePresenter.ts
+++ nextjs/src/features/galaxyInterface/components/GalaxyWrapper/Suggestions/usePresenter.ts
@@ -1,5 +1,4 @@
 import { State } from '@/features/shared/configs/store'
-import { useTypeSafeTranslation } from '@/features/shared/hooks/translations'
 import { sharedActions } from '@/features/shared/stores/shared.store'
 import { SearchCategory } from '@/features/shared/utils/categories'
 
@@ -10,10 +9,7 @@ export const usePresenter = () => {
 
   const { isCategorySuggestionsOpen } = useSelector((state: State) => state.shared)
 
-  const { t } = useTypeSafeTranslation('category')
-
   return {
-    t,
     isCategorySuggestionsOpen,
     setSearchCategory: (category: SearchCategory) => {
       dispatch(sharedActions.searchCategory({ searchCategory: category }))
