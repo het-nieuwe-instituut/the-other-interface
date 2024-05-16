b8423e7c0545af69cadc7920a202e05883588f42
Rick Woltheus <rwoltheus@gmail.com>
2024-05-16 16:39:16 +0200

    simplify even more

--- nextjs/src/features/galaxyInterface/galaxies/DetailGalaxy/fragments/Record/Record.tsx
+++ nextjs/src/features/galaxyInterface/galaxies/DetailGalaxy/fragments/Record/Record.tsx
@@ -11,34 +11,22 @@ import { TypographyVariants } from '@/features/ui/components/typography/variants
 
 type Props = {
   record: PositionedRecord
-  onHoverRecord?: () => void
-  onLeaveRecord?: () => void
-  isHovered?: boolean
-  currentRecord?: string
   tabIndex?: number
   style?: React.CSSProperties
 }
 
-export const Record: React.FC<Props> = ({
-  record,
-  style,
-  onHoverRecord,
-  onLeaveRecord,
-  isHovered,
-  currentRecord,
-  tabIndex = 0,
-}) => {
+export const Record: React.FC<Props> = ({ record, style, tabIndex = 0 }) => {
   const { id, category, position, grid } = record
   const { recordDetails, isLoading, handleClick } = usePresenter(id, category)
   // TODO: HNIT-1833 - add in loading and error handling
   const { data } = useZoomHoverRecordResultQuery({ id, category })
-  const line = useCalculateLine()
+  const { coordinates, ref } = useCalculateLine()
 
   if (!recordDetails && !isLoading) return null
 
   return (
     <>
-      <div className="relative" style={{ ...grid, ...style }}>
+      <div className="relative peer" style={{ ...grid, ...style }}>
         <Tooltip
           isDisabled={!data}
           label={
@@ -51,15 +39,13 @@ export const Record: React.FC<Props> = ({
           }
         >
           <button
+            ref={ref}
             tabIndex={tabIndex}
             className={cn(
-              ' ease-in-out duration-[.4] transition-all absolute flex size-4/5 cursor-pointer flex-col items-center justify-center gap-[2px] hover:scale-105',
-              isHovered && currentRecord === id ? `hovered` : ''
+              'ease-in-out duration-[.4] transition-all absolute flex size-4/5 cursor-pointer flex-col items-center justify-center gap-[2px] hover:scale-105',
+              'use-calculate-line-element',
+              'group-hover-blur-item'
             )}
-            onMouseOver={onHoverRecord}
-            onFocus={onHoverRecord}
-            onMouseLeave={onLeaveRecord}
-            onBlur={onLeaveRecord}
             onClick={handleClick}
             style={{ ...position }}
           >
@@ -78,29 +64,29 @@ export const Record: React.FC<Props> = ({
           </button>
         </Tooltip>
       </div>
-      {isHovered && currentRecord === id && (
-        <div
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            zIndex: -80,
-          }}
-        >
-          <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
-            <line
-              x1={line.x1}
-              y1={line.y1}
-              x2={line.x2}
-              y2={line.y2}
-              strokeWidth={1}
-              stroke={'RGBA(0, 81, 255, .4)'}
-            />
-          </svg>
-        </div>
-      )}
+
+      <div
+        className="hidden peer-hover:block"
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          zIndex: -80,
+        }}
+      >
+        <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+          <line
+            x1={coordinates.x1}
+            y1={coordinates.y1}
+            x2={coordinates.x2}
+            y2={coordinates.y2}
+            strokeWidth={1}
+            stroke={'RGBA(0, 81, 255, .4)'}
+          />
+        </svg>
+      </div>
     </>
   )
 }
